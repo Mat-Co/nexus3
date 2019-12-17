2b0d998055b584aaa2281028bdb9b115403b59a6
Baptiste Courtois <b.courtois@criteo.com>
2019-12-17 22:31:46 +0100

    Refactor inspec's integration tests for nexus3 resources
    Setup integration tests by instanciating all resources in the recipe:
       nexus3_test::integration
    Use dedicated data to ensure resource tests are not conflicting with
    the inspec ones.
    Simplify integration tests by using inspec http helper.

--- test/integration/default/inspec/inspec_helper.rb
+++ test/integration/default/inspec/inspec_helper.rb
@@ -1,3 +1,13 @@
-unless (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM).nil?
-  set :os, family: 'windows'
+CREDS = { user: 'admin', pass: 'admin123' }.freeze
+SCRIPT_API = 'http://localhost:8081/service/rest/v1/script'.freeze
+JSON_HEADERS = { 'Content-Type' => 'application/json' }.freeze
+TEXT_HEADERS = { 'Content-Type' => 'plain/text' }.freeze
+
+def nexus3_json_script_http(endpoint, data)
+  require 'json'
+  nexus3_script_http(endpoint, data.to_json, JSON_HEADERS)
+end
+
+def nexus3_script_http(endpoint, data, headers = TEXT_HEADERS)
+  http("#{SCRIPT_API}/#{endpoint}", auth: CREDS, method: 'POST', headers: headers, data: data)
 end
